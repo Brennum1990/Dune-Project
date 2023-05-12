82957eb746c56fc45534053b5c8305f00b44c1af
Brennum1990 <111984560+Brennum1990@users.noreply.github.com>
2023-05-12 02:10:47 +0200

    Added accordion to sidebar

---
+++ _app/js/modules/show-hide.js
@@ -0,0 +1,54 @@
+export default function showHide() {
+
+	// Planets
+	const planetContainer = document.querySelector('.planet__container');
+	const planetBtn = document.querySelector('.planet'); 
+	
+	function showPlanets() {
+		if (planetContainer.style.display === "none") {
+			planetContainer.style.display = "block";
+		} else {
+		  planetContainer.style.display = "none";
+		} 
+	}
+	planetBtn.addEventListener('click', showPlanets);
+
+	// Characters 
+	const characterContainer = document.querySelector('.character__container');
+	const characterBtn = document.querySelector('.character'); 
+	
+	function showCharacters() {
+		if (characterContainer.style.display === "none") {
+			characterContainer.style.display = "block";
+		} else {
+		  characterContainer.style.display = "none";
+		} 
+	}
+	characterBtn.addEventListener('click', showCharacters);
+
+	// Houses 
+	const houseContainer = document.querySelector('.house__container');
+	const houseBtn = document.querySelector('.house'); 
+	
+	function showHouses() {
+		if (houseContainer.style.display === "none") {
+			houseContainer.style.display = "block";
+		} else {
+		  houseContainer.style.display = "none";
+		} 
+	}
+	houseBtn.addEventListener('click', showHouses);
+
+	// Organizations & Groups 
+	const groupContainer = document.querySelector('.organization__container');
+	const groupBtn = document.querySelector('.organization'); 
+	
+	function showGroups() {
+		if (groupContainer.style.display === "none") {
+			groupContainer.style.display = "block";
+		} else {
+		  groupContainer.style.display = "none";
+		} 
+	}
+	groupBtn.addEventListener('click', showGroups);
+}
